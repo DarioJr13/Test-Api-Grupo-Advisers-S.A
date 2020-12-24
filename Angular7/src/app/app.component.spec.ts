6fed1b6fda68a5c5c9b07fec39b1d2c2309f0ef2
Dario Esquina <dario13esquina@gmail.com>
2020-12-24 00:11:36 -0500

    Test
    Test

---
+++ Angular7/src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('debería crear la aplicación', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`debería tener como título'Angular7'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Angular7');
+  });
+
+  it(' debería representar el título en un h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Bienvenido a Angular7!');
+  });
+});
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Test-Api-Grupo-Advisers-S.A
- Test
+# Api-Grupo-Advisers
+ Prueba-Técnica
+
+ Darío Esquina
+
+ Para correr este proyecto
+
+ Necesita tener Sql Server Minimo 2014 
+ Visual Studio 2015 si tiene el 2019 mucho mejor
+ Angular y cualquier editor de texto
+
+DB CONFIGURACIÓN
+ * Lo primero es crear la Base de datos RestaurantDB.
+ ** Luego correr el Script de la base de datos.
+
+ BACK-END
+ *** Una vez tenido nuestra DB procedemos a presionar doble Click a nuestra solución que es WebAPI.sln
+ **** Corremos nuestro servidor
+
+ Front-End 
+ ***** npm i para descargar nuestras dependencias.
+ ****** ng serve
+
+ Listo estara funcionando nuestra App.
+
+ Espero le haya servido cualquier duda
+Correo dario13esquina@gmail.com
+ Teléfono 507-66649941
